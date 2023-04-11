95f9ccdae5b330c39aee986d756d68e8e64837fa
Sinus <sinus@sinpi.net>
2023-04-11 22:03:47 +0200

    MAYBE fixed pesky  issues with ! or % in filenames.

--- lrcat_diff.bat
+++ lrcat_diff.bat
@@ -1,20 +1,30 @@
 @echo off 
-setlocal EnableDelayedExpansion
 
-if [%1]==[] ( echo ERROR: I need two .lrcat filenames to compare. && exit -b 1 )
 if [%2]==[] ( echo ERROR: I need two .lrcat filenames to compare. && exit -b 1 )
-if not exist %1 ( echo ERROR: %1 doesn't exist. && exit -b 2 )
-if not exist %2 ( echo ERROR: %2 doesn't exist. && exit -b 2 )
+if not exist "%~1" ( echo ERROR: %1 doesn't exist. && exit -b 2 )
+if not exist "%~2" ( echo ERROR: %2 doesn't exist. && exit -b 2 )
 
+setlocal DisableDelayedExpansion
+SET "FN1=%1"
+setlocal EnableDelayedExpansion
+SET FN1U=!FN1: =%%20!
+setlocal DisableDelayedExpansion
+SET "FN2=%2"
+setlocal EnableDelayedExpansion
+SET FN2U=!FN2: =%%20!
+
+setlocal DisableDelayedExpansion
+echo %FN1U%
+echo %FN2U%
 SET query=^
  ^
 ATTACH 'file::memory:' as tmp; ^
  ^
-ATTACH 'file:%1?readonly=1^&immutable=1' AS lrcat1; ^
+ATTACH 'file:%FN1U%^?readonly=1^&immutable=1' AS lrcat1; ^
 DROP TABLE IF EXISTS tmp.file1; ^
 CREATE TABLE tmp.file1 AS ^
 SELECT ^
-  hs.id_global AS id, ^
+  adobe_images.id_global AS id, ^
   AgLibraryFolder.pathFromRoot^|^|AgLibraryFile.baseName AS path, ^
   MAX(DATETIME(hs.dateCreated + 978310800 + 3600,'unixepoch')) AS date, ^
   CASE touchTime WHEN 0 THEN "0" ELSE DATETIME(touchTime + 978310800 + 3600,'unixepoch') END AS tdate, ^
@@ -24,15 +34,15 @@ FROM ^
 LEFT JOIN adobe_images on adobe_images.id_local=hs.image ^
 LEFT JOIN AgLibraryFile on AgLibraryFile.id_local=adobe_images.rootFile ^
 LEFT JOIN AgLibraryFolder on AgLibraryFolder.id_local=AgLibraryFile.folder ^
-GROUP BY adobe_images.id_global ^
+GROUP BY id ^
 ORDER BY date DESC; ^
 DETACH lrcat1; ^
  ^
-ATTACH 'file:%2?readonly=1^&immutable=1' AS lrcat2; ^
+ATTACH 'file:%FN2U%^?readonly=1^&immutable=1' AS lrcat2; ^
 DROP TABLE IF EXISTS tmp.file2; ^
 CREATE TABLE tmp.file2 AS ^
 SELECT ^
-  hs.id_global AS id, ^
+  adobe_images.id_global AS id, ^
   AgLibraryFolder.pathFromRoot^|^|AgLibraryFile.baseName AS path, ^
   MAX(DATETIME(hs.dateCreated + 978310800 + 3600,'unixepoch')) AS date, ^
   CASE touchTime WHEN 0 THEN "0" ELSE DATETIME(touchTime + 978310800 + 3600,'unixepoch') END AS tdate, ^
@@ -42,7 +52,7 @@ FROM ^
 LEFT JOIN adobe_images on adobe_images.id_local=hs.image ^
 LEFT JOIN AgLibraryFile on AgLibraryFile.id_local=adobe_images.rootFile ^
 LEFT JOIN AgLibraryFolder on AgLibraryFolder.id_local=AgLibraryFile.folder ^
-GROUP BY adobe_images.id_global ^
+GROUP BY id ^
 ORDER BY date DESC; ^
 DETACH lrcat2; ^
  ^
@@ -76,7 +86,7 @@ SELECT path, ^
 %~dp0sqlite3.exe -csv -header :memory: "%query%" >%TEMP%\sql.csv
 
 if errorlevel 1 goto err
-start powershell -WindowStyle hidden -command "get-content -Encoding UTF8 %TEMP%\sql.csv |convertfrom-csv |out-gridview -title 'left:%1 right:%2' -wait; remove-item '%TEMP%\sql.csv'"
+start powershell -WindowStyle hidden -command "get-content -Encoding UTF8 %TEMP%\sql.csv |convertfrom-csv |out-gridview -title '%1 <-> %2' -wait; remove-item '%TEMP%\sql.csv'"
 rem format-table @{e='diff';width=7},@{e='path';width=30},tdate1,val1,tdate2,val2 -wrap |out-host  $Host.ui.RawUI.WindowTitle='%1'; 
 goto :end
 
